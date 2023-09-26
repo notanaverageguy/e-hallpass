b2aa62425819e10ab130b771e0e165b11af9000f
orosado844@gmail.com <83960401+notanaverageguy@users.noreply.github.com>
2023-09-25 21:55:21 -0400

    update Navbar.svelte and +page.svelte

--- src/routes/(Main App)/Navbar.svelte
+++ src/routes/(Main App)/Navbar.svelte
@@ -1,62 +1,60 @@
 <script lang="ts">
-    import profileImg from '$lib/Navbar Images/Profile.png';
-    import notificationImg from '$lib/Navbar Images/Notification.png';
-    import helpImg from '$lib/Navbar Images/Help.png';
+	import profileImg from '$lib/Navbar Images/Profile.png';
+	import notificationImg from '$lib/Navbar Images/Notification.png';
+	import helpImg from '$lib/Navbar Images/Help.png';
 
-    let header: string;
+	let header: string = 'Unknown area';
 
-    import { onMount } from 'svelte';
-    onMount(() => {
-        const url = window.location.href;
-        const route = url.replace(/(?:http:\/\/(?:.)+)?(?:[0-9]\.?)+\//, "");
-        switch(route) {
-            case "id-card":
-                header = "Digital ID";
-                break;
-            case "passes/activepass":
-                header = "Active Pass";
-                break;
-            case "passes/create":
-                header = "Create Pass";
-                break;
-            case "appointments":
-                header = "Appointments";
-                break;
-            case "updates":
-                header = "Updates";
-                break;
-            case "user-settings":
-                header = "User Settings";
-                break;
-            default:
-                header = route;
-        }
-    });
+	import { onMount } from 'svelte';
+	onMount(() => {
+		const url = window.location.href;
+		const route = url.replace(/(?:http:\/\/(?:.)+)?(?:[0-9]\.?)+\//, '');
 
+		var testers: any = {
+            'id-card': 'Digital ID',
+			'passes\/create': 'Create Pass',
+            'passes\/activepass': 'Active Pass',
+            'appointments': 'Appointments',
+            'updates': 'Updates',
+            'training\/documents': 'Documents',
+            'training\/videos': 'Training Videos',
+            'user-settings': 'User Settings',
+		};
+
+		for (const test in testers) {
+			if (testers.hasOwnProperty(test)) {
+                const regex = new RegExp(test, 'g' )
+                if(regex.exec(route) !== null) {header = testers[test]; console.log('match')} else {
+                    console.log(regex, route, regex.exec(route))
+                }
+			}
+		}
+
+	});
 </script>
 
 <div class="bg-white z-0">
 	<div class="p-0 p-lg-3 flex-column-reverse">
-        <ul class="py-2 px-2 px-lg-0 py-md-0 min-h-[63px] flex items-center border-b border-[#d8dbe0]">
-            <div class="me-auto">
-                <div class="ml-[24px] text-[18px]">
-                    {#if header}
-                        { header }
-                    {/if}
-                </div>
-            </div>
-            <!--Buttons-->
-            <div class="relative right-0 flex items-center">
-                <a href="/id-card">
-                    <img src="{profileImg}" alt="wtv" width=22 class="mx-2">
-                </a>
-                <div>
-                    <img src="{notificationImg}" alt="wtv" width=20 class="mx-2">
-                </div>
-                <button>
-                    <img src="{helpImg}" alt="wtv" width=15 class="mx-2">
-                </button>
-            </div>
-        </ul>
-    </div>
+		<ul class="py-2 px-2 px-lg-0 py-md-0 min-h-[63px] flex items-center border-b border-[#d8dbe0]">
+			<div class="me-auto">
+				<div class="ml-[24px] text-[18px]">
+					{#if header}
+						{header}
+					{/if}
+				</div>
+			</div>
+			<!--Buttons-->
+			<div class="relative right-0 flex items-center">
+				<a href="/id-card">
+					<img src={profileImg} alt="wtv" width="22" class="mx-2" />
+				</a>
+				<div>
+					<img src={notificationImg} alt="wtv" width="20" class="mx-2" />
+				</div>
+				<button>
+					<img src={helpImg} alt="wtv" width="15" class="mx-2" />
+				</button>
+			</div>
+		</ul>
+	</div>
 </div>
--- src/routes/(Main App)/passes/create/+page.svelte
+++ src/routes/(Main App)/passes/create/+page.svelte
@@ -99,101 +99,104 @@
                 </div>
 
                 <div class="my-2">
-                    <span class="opacity-50 text-[21px] mb-2 inline-block font-normal">Departing From:</span>
+                    <form method="POST" action="?/submit">
 
-                    <div class="p-[11.2px] border border-[#003b75]">
-                        <div class="px-[2px] flex h-[24px]">
-                            <input class="w-[339px] outline-0" placeholder="Teacher/location" bind:value= { departingSort } on:focus={ departingFocus } on:focusout = { departingUnfocus } >
+                        <span class="opacity-50 text-[21px] mb-2 inline-block font-normal">Departing From:</span>
+    
+                        <div class="p-[11.2px] border border-[#003b75]">
+                            <div class="px-[2px] flex h-[24px]">
+                                <input name="depart" class="w-[339px] outline-0" placeholder="Teacher/location" bind:value= { departingSort } on:focus={ departingFocus } on:focusout = { departingUnfocus } >
+                            </div>
+                            <div id="departingDropDown" class="hidden absolute mt-3 right-0 left-0 mx-[15px] bg-white max-h-[148px] bg-white z-10 overflow-y-auto border-b border-r border-black">
+                                {#each locations as location}
+                                    {#if location.name.toLowerCase().includes(departingSort.toLowerCase())}
+                                        <div class="truncate py-2 px-4 text-sm {location.name == departingInput ? "bg-gradient-to-r from-[#298FCE] to-[#304BAF] text-white" : ""}">
+                                            <button  class="w-[100%] text-left" on:click|preventDefault = { () => {departingSort = `${location.name}`}}>
+                                                <p>{ location.name }{ location.email ? `, ${location.email}` : "" }</p>
+                                            </button>
+                                        </div>
+                                    {/if}
+                                {/each}
+                            </div>
                         </div>
-                        <div id="departingDropDown" class="hidden absolute mt-3 right-0 left-0 mx-[15px] bg-white max-h-[148px] bg-white z-10 overflow-y-auto border-b border-r border-black">
-                            {#each locations as location}
-                                {#if location.name.toLowerCase().includes(departingSort.toLowerCase())}
-                                    <div class="truncate py-2 px-4 text-sm {location.name == departingInput ? "bg-gradient-to-r from-[#298FCE] to-[#304BAF] text-white" : ""}">
-                                        <button class="w-[100%] text-left" on:click= { () => {departingSort = `${location.name}`}}>
-                                            <p>{ location.name }{ location.email ? `, ${location.email}` : "" }</p>
-                                        </button>
-                                    </div>
-                                {/if}
-                            {/each}
+    
+                        <div class="text-blue-700"><!--EDIT BUTTON-->
+                            <span class="text-50 flex justify-end relative z-0">
+                                <svg class="mr-[8px]" width=20 fill="#1d4ed8" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M199.04 672.64l193.984 112 224-387.968-193.92-112-224 388.032z m-23.872 60.16l32.896 148.288 144.896-45.696-177.792-102.592zM455.04 229.248l193.92 112 56.704-98.112-193.984-112-56.64 98.112zM104.32 708.8l384-665.024 304.768 175.936-383.936 665.088h0.064l-248.448 78.336-56.448-254.336z m384 254.272v-64h448v64h-448z"></path></g></svg>
+                                Edit
+                            </span>
                         </div>
-                    </div>
-
-                    <div class="text-blue-700"><!--EDIT BUTTON-->
-                        <span class="text-50 flex justify-end relative z-0">
-                            <svg class="mr-[8px]" width=20 fill="#1d4ed8" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M199.04 672.64l193.984 112 224-387.968-193.92-112-224 388.032z m-23.872 60.16l32.896 148.288 144.896-45.696-177.792-102.592zM455.04 229.248l193.92 112 56.704-98.112-193.984-112-56.64 98.112zM104.32 708.8l384-665.024 304.768 175.936-383.936 665.088h0.064l-248.448 78.336-56.448-254.336z m384 254.272v-64h448v64h-448z"></path></g></svg>
-                            Edit
-                        </span>
-                    </div>
-
-                    <div class="flex py-[10px] overflow-x-auto overflow-y-clip snap-x"><!--Favorite teacher/room thing-->
-                        {#each favoriteDepart as location}                            
-                            <div class="h-[100px] snap-center">
-                                <div class="flex pl-[5px]">
-                                    <div class="px-[5px] w-1/3">
-                                        <div class="h-[100px] w-[28vw] rounded-xl flex flex-col justify-center border border-[#003b75]">
-                                            <div class="mt-[4px] mb-[2px] flex justify-center items-center pt-[4px] pb-[2px] mt-[30px]">
-                                                <img src={plugImg} alt="" width=30>
+    
+                        <div class="flex py-[10px] overflow-x-auto overflow-y-clip snap-x"><!--Favorite teacher/room thing-->
+                            {#each favoriteDepart as location}                            
+                                <div class="h-[100px] snap-center">
+                                    <div class="flex pl-[5px]">
+                                        <div class="px-[5px] w-1/3">
+                                            <div class="h-[100px] w-[28vw] rounded-xl flex flex-col justify-center border border-[#003b75]">
+                                                <div class="mt-[4px] mb-[2px] flex justify-center items-center pt-[4px] pb-[2px] mt-[30px]">
+                                                    <img src={plugImg} alt="" width=30>
+                                                </div>
+                                                <span class="p-[20px] text-[8px] font-bold text-center uppercase text-[#003b75]"> Add favorite Teacher/Room </span>
                                             </div>
-                                            <span class="p-[20px] text-[8px] font-bold text-center uppercase text-[#003b75]"> Add favorite Teacher/Room </span>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        {/each}
-                    </div>
-                    
-                    <hr class="my-[16px] border-t border-gray-300">
-
-                    <span class="opacity-50 text-[21px] mb-2 inline-block font-normal">Destination:</span>
-
-                    <div class="p-[11.2px] border border-[#003b75]">
-                        <div class="px-[2px] flex h-[24px]">
-                            <input class="w-[339px] outline-0" placeholder="Teacher/location" bind:value= { destSort } on:focus={ destFocus } on:focusout = { destUnfocus } >
-                        </div>
-                        <div id="destDropDown" class="hidden absolute mt-3 right-0 left-0 mx-[15px] bg-white max-h-[148px] bg-white z-10 overflow-y-auto border-b border-r border-black">
-                            {#each locations as location}
-                                {#if location.name.toLowerCase().includes(destSort.toLowerCase())}
-                                    <div class="truncate py-2 px-4 text-sm {location.name == destInput ? "bg-gradient-to-r from-[#298FCE] to-[#304BAF] text-white" : ""}">
-                                        <button class="w-[100%] text-left" on:click= { () => {destSort = `${location.name}`}}>
-                                            <p>{ location.name }{ location.email ? `, ${location.email}` : "" }</p>
-                                        </button>
-                                    </div>
-                                {/if}
                             {/each}
                         </div>
-                    </div>
-
-                    <div class="text-blue-700"><!--EDIT BUTTON-->
-                        <span class="text-50 flex justify-end relative z-0">
-                            <svg class="mr-[8px]" width=20 fill="#1d4ed8" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M199.04 672.64l193.984 112 224-387.968-193.92-112-224 388.032z m-23.872 60.16l32.896 148.288 144.896-45.696-177.792-102.592zM455.04 229.248l193.92 112 56.704-98.112-193.984-112-56.64 98.112zM104.32 708.8l384-665.024 304.768 175.936-383.936 665.088h0.064l-248.448 78.336-56.448-254.336z m384 254.272v-64h448v64h-448z"></path></g></svg>
-                            Edit
-                        </span>
-                    </div>
-
-                    <div class="flex py-[10px] overflow-x-auto overflow-y-clip snap-x"><!--Favorite teacher/room thing-->
-                        {#each favoriteDest as location}                            
-                            <div class="h-[100px] snap-center">
-                                <div class="flex pl-[5px]">
-                                    <div class="px-[5px] w-1/3">
-                                        <div class="h-[100px] w-[28vw] rounded-xl flex flex-col justify-center border border-[#003b75]">
-                                            <div class="mt-[4px] mb-[2px] flex justify-center items-center pt-[4px] pb-[2px] mt-[30px]">
-                                                <img src={plugImg} alt="" width=30>
+                        
+                        <hr class="my-[16px] border-t border-gray-300">
+    
+                        <span class="opacity-50 text-[21px] mb-2 inline-block font-normal">Destination:</span>
+    
+                        <div class="p-[11.2px] border border-[#003b75]">
+                            <div class="px-[2px] flex h-[24px]">
+                                <input class="w-[339px] outline-0" name="dest" placeholder="Teacher/location" bind:value= { destSort } on:focus={ destFocus } on:focusout = { destUnfocus } >
+                            </div>
+                            <div id="destDropDown" class="hidden absolute mt-3 right-0 left-0 mx-[15px] bg-white max-h-[148px] bg-white z-10 overflow-y-auto border-b border-r border-black">
+                                {#each locations as location}
+                                    {#if location.name.toLowerCase().includes(destSort.toLowerCase())}
+                                        <div class="truncate py-2 px-4 text-sm {location.name == destInput ? "bg-gradient-to-r from-[#298FCE] to-[#304BAF] text-white" : ""}">
+                                            <button class="w-[100%] text-left" on:click|preventDefault= { () => {destSort = `${location.name}`}}>
+                                                <p>{ location.name }{ location.email ? `, ${location.email}` : "" }</p>
+                                            </button>
+                                        </div>
+                                    {/if}
+                                {/each}
+                            </div>
+                        </div>
+    
+                        <div class="text-blue-700"><!--EDIT BUTTON-->
+                            <span class="text-50 flex justify-end relative z-0">
+                                <svg class="mr-[8px]" width=20 fill="#1d4ed8" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M199.04 672.64l193.984 112 224-387.968-193.92-112-224 388.032z m-23.872 60.16l32.896 148.288 144.896-45.696-177.792-102.592zM455.04 229.248l193.92 112 56.704-98.112-193.984-112-56.64 98.112zM104.32 708.8l384-665.024 304.768 175.936-383.936 665.088h0.064l-248.448 78.336-56.448-254.336z m384 254.272v-64h448v64h-448z"></path></g></svg>
+                                Edit
+                            </span>
+                        </div>
+    
+                        <div class="flex py-[10px] overflow-x-auto overflow-y-clip snap-x"><!--Favorite teacher/room thing-->
+                            {#each favoriteDest as location}                            
+                                <div class="h-[100px] snap-center">
+                                    <div class="flex pl-[5px]">
+                                        <div class="px-[5px] w-1/3">
+                                            <div class="h-[100px] w-[28vw] rounded-xl flex flex-col justify-center border border-[#003b75]">
+                                                <div class="mt-[4px] mb-[2px] flex justify-center items-center pt-[4px] pb-[2px] mt-[30px]">
+                                                    <img src={plugImg} alt="" width=30>
+                                                </div>
+                                                <span class="p-[20px] text-[8px] font-bold text-center uppercase text-[#003b75]"> Add favorite Teacher/Room </span>
                                             </div>
-                                            <span class="p-[20px] text-[8px] font-bold text-center uppercase text-[#003b75]"> Add favorite Teacher/Room </span>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        {/each}
-                    </div>
-
-                    <div class="flex justify-center">
-                        <div class="mt-[8px] mb-[16px]">
-                            <button class="mt-[8px] py-[16px] px-[32px] bg-black text-white font-bold rounded-xl bg-gradient-to-r from-[#003975] to-[#005dba]">
-                                <p class="text-[14px]">Submit</p>
-                            </button>
+                            {/each}
                         </div>
-                    </div>
+    
+                        <div class="flex justify-center">
+                            <div class="mt-[8px] mb-[16px]">
+                                <button class="mt-[8px] py-[16px] px-[32px] bg-black text-white font-bold rounded-xl bg-gradient-to-r from-[#003975] to-[#005dba]">
+                                    <p class="text-[14px]">Submit</p>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
 
             </div>
