b215a6fd8e9f3c64585418b0adc87e0e4b87c486
orosado844@gmail.com <83960401+notanaverageguy@users.noreply.github.com>
2023-09-27 15:37:45 -0400

    update +page.svelte and app.css. Added fonts

--- src/routes/(Main App)/passes/activepass/+page.svelte
+++ src/routes/(Main App)/passes/activepass/+page.svelte
@@ -9,4 +9,19 @@
     
 </script>
 
-{ depart } { dest } { time }
+<div class="pt-[32px] pb-[15px] h-[100%]">
+    <div class="px-[12px] bg-gray-300">
+        <div class="rounded-lg pt-[8px]">
+
+            <div class="m-[16px]">
+                <div class="px-[30px] py-[40px] bg-white rounded-2xl flex align-center justify-center w-[100%] shadow text-red-500 text-[50px]" style="font-family: Metropolis-Bold">
+                    00:03:17 
+                </div>
+            </div>
+
+            <hr class="my-[16px] border-t bg-white h-[2px]">
+
+            rawr
+        </div>
+    </div>
+</div>
